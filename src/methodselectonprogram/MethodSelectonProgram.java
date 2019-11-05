e9efef86c324e5db0805617d7f7428a97d37f2a3
Kieran Bates <>
2019-11-05 13:54:26 -0500

    First commit of MethodSelectionProgram

---
+++ src/methodselectonprogram/MethodSelectonProgram.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package methodselectonprogram;
+
+/**
+ *
+ * @author kibat6204
+ */
+public class MethodSelectonProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void vpQualification(String one)
+    {
+        String[] letters = {"A", "B", "C", "D", "E", "F", "G"};
+        boolean firstSearch = true;
+        boolean secondSearch = true;
+        boolean thirdSearch = true;
+        while()
+    }
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
